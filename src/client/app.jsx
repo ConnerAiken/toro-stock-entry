2838c21395d93f4895488f3b3430c768edc71229
ConnerAiken <conner@fittedtech.com>
2019-03-23 13:51:22 -0700

    Init to win it

---
+++ src/client/app.jsx
@@ -0,0 +1,31 @@
+// Node.JS
+import React from "react";
+import ReactDOM from "react-dom";
+  
+class App extends React.Component {
+
+  constructor(props) {
+    super(props); 
+  }
+
+  handleStockSubmit(event) {
+    console.log(event);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Stock Entry Form</h2>
+        <input type="text" id="stock"/>
+        <button onClick={this.handleStockSubmit.bind(this)}>Save</button>
+      </div>
+    );
+  }
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+
+module.hot.accept();
---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import utils from "./helpers/utils.js";
+import path from "path";
+import dotenv from "dotenv";
+
+global.path = path;
+global.dotenv = dotenv;
+
+utils.loadENV();
+const app = express();
+
+app.use(express.static(path.resolve(process.cwd(), 'public')))
+
+app.get('/stock', (req, res) => {
+    console.log(req);
+});
+
+app.listen(process.env.port, () => {
+    utils.log(`Server has started and is listening on port ${process.env.port}!`)
+});
